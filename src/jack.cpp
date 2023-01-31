ceb7472d2b0f8af453ac41d69587a17e6f12d86a
ebritten <ebritten@csu.fullerton.edu>
2023-01-31 14:00:02 -0800

    feat: add basic main function

---
+++ src/jack.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Eric Britten on 1/31/23.
+//
+
+#include "jack.h"
---
+++ src/jack.h
@@ -0,0 +1,8 @@
+//
+// Created by Eric Britten on 1/31/23.
+//
+
+#ifndef JACK_JACK_H
+#define JACK_JACK_H
+
+#endif //JACK_JACK_H
